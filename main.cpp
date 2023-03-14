157915149ca2898d9d0d72c5c16617f821c4da0d
aaaxyettt <bedy.s.boshkoy@yandex.ru>
2023-03-14 13:17:10 +0300

    file error added

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 #include <stdio.h>
 
 int main(int argc, char** argv){
-	
+	if(argc == 1){
+		std::cout << "no file(" << std::endl;
+		return 0;
+	}
+
 	FILE* f = fopen(argv[1], "r");
 	check_polindrome(f);
 	fclose(f);
